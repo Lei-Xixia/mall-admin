9427e934bb7514ea121253726ad3c10d9af96bef
Lei-Xixia <1377158174@qq.com>
2021-12-16 17:28:24 +0800

    完成新增商品基本信息页面

--- src/views/page/productAdd.vue
+++ src/views/page/productAdd.vue
@@ -1,9 +1,64 @@
 <template>
-  <h2>新增商品</h2>
+  <div class="product-detail">
+    <a-steps :current="current" class="product-step">
+      <a-step v-for="item in steps" :key="item.title" :title="item.title" />
+    </a-steps>
+    <div class="steps-content">
+      <BasicDetail v-if="current === 0" @next="next"/>
+      <SaleDetail v-else-if="current === 1" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import BasicDetail from "@/components/basicDetail.vue";
+import SaleDetail from "@/components/saleDetail.vue";
+export default {
+  components: {
+    BasicDetail,
+    SaleDetail,
+  },
+  data() {
+    return {
+      current: 0,
+      steps: [
+        {
+          title: "填写商品基本信息",
+        },
+        {
+          title: "填写商品销售信息",
+        },
+      ],
+    };
+  },
+  methods: {
+    next() {
+      this.current++;
+    },
+    prev() {
+      this.current--;
+    },
+  },
+};
 </script>
 
-<style></style>
+<style lang="less">
+.product-detail {
+  .product-step{
+    width: 50%;
+    margin: 20px auto;
+  }
+  .steps-content {
+    margin-top: 16px;
+    border: 1px dashed #e9e9e9;
+    border-radius: 6px;
+    background-color: #fafafa;
+    min-height: 200px;
+    text-align: center;
+    padding-top: 80px;
+  }
+  .steps-action {
+    margin-top: 24px;
+  }
+}
+</style>
